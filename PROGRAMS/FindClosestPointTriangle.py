000dd417c194d3ab8453524d4714508f2f0d7a6d
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-11-16 18:56:52 -0500

    Rough find_closest_point

--- PROGRAMS/FindClosestPointTriangle.py
+++ PROGRAMS/FindClosestPointTriangle.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
+from numpy.linalg import lstsq
 
-# Finds closest point on triangle
+# Finds closest point on triangle by solving least squares using numpy.linalg's lstsq method
+# Given point a and triangle coordinates p, q, r find closest point
 
-def triangle_find_closest(file):
+def find_closest_point(a, triangle_vertices):
+    # Check if point inside triangle
+    p, q, r = triangle_vertices[0], triangle_vertices[1], triangle_vertices[2]
+    A = a - p
+    B = lamb * (q - p)
+    C = mu * (r - p)
 
+    # Check if point on triangle border
 
     return vertices, triangle_vertex_coords
