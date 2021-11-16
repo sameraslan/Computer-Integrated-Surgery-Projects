3043ca6ddd8c8ec59f9c13423985a8423b44dede
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-11-16 17:38:59 -0500

    test

--- PROGRAMS/ReadSurfaceMesh.py
+++ PROGRAMS/ReadSurfaceMesh.py
@@ -7,20 +7,20 @@ def read_surfacemesh(file):
     # reads in first line of number of vertices as int
     num_vertices = (pd.read_csv(file, sep=" ", header=None, nrows=1))[0][0]
 
-    # reads in vertices into (numVertices)
+    # reads in vertices into (numVertices) xyz coordinates of vertices in CT coordinates
     vertices = pd.read_csv(file, sep=" ", header=None, skiprows=lambda x: x not in range(1, num_vertices + 1))
 
     # reads in number of triangles as int
     num_triangles = (pd.read_csv(file, sep=" ", header=None, skiprows=lambda x: x not in [num_vertices + 1]))[0][0]
 
     # reads in triangles into 3135 (numTriangles) x 6 dataframe
-    vertices = pd.read_csv(file, sep=" ", header=None,
+    triangle_vertices = pd.read_csv(file, sep=" ", header=None,
                            skiprows=lambda x: x not in range(num_vertices + 2, num_triangles + num_vertices + 2))
 
     # modifies vertices to get only first 3 columns which is the vertex coordinates of the triangles as pandas dataframe
-    triangle_vertex_coords = vertices[[0, 1, 2]]
+    triangle_vertex_coords = triangle_vertices[[0, 1, 2]]
 
-    print(triangle_vertex_coords)
+    print(np.array(triangle_vertex_coords))
 
     # N_D = int(calreadings[:1][0][0])
     # N_A = int(calreadings[:1][1][0])
